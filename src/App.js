2002a7e5edc0d2394d49bd8a1d5840ab45f7237f
unknown <habibighofrane0000@gmail.com>
2021-12-02 20:38:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Profile} from './profile/Profile';
 
 function App() {
-  return (
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile 
+      fullName= "ghofrane"
+      Bio= "this is my bio"
+      Profission="student">
+        <img src= " https://www.w3schools.com/howto/img_avatar2.png " alt="profile pic"></img>
+      </Profile>
+
     </div>
   );
 }
